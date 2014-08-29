add61eb64f4d7bd6563c8e21fd01bf1e2a39e8cb
Xinyue Zhao <czhao@cellular.de>
2014-08-29 14:14:40 +0200

    Added animation for listview and gridview.

--- app/src/main/java/com/schautup/fragments/ScheduleListFragment.java
+++ app/src/main/java/com/schautup/fragments/ScheduleListFragment.java
@@ -7,6 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.TranslateAnimation;
 import android.widget.ListView;
 
 import com.schautup.R;
@@ -47,6 +52,17 @@ public final class ScheduleListFragment extends BaseListFragment {
 	@Override
 	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 		ListView listView = (ListView) view.findViewById(R.id.schedule_lv);
+
+		AnimationSet set = new AnimationSet(true);
+		Animation animation = new AlphaAnimation(0.0f, 1.0f);
+		animation.setDuration(500);
+		set.addAnimation(animation);
+		animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
+				Animation.RELATIVE_TO_SELF, -1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+		animation.setDuration(100);
+		set.addAnimation(animation);
+		LayoutAnimationController controller = new LayoutAnimationController(set, 0.5f);
+		listView.setLayoutAnimation(controller);
 		setListViewWidget(listView);
 		setAdapter(new ScheduleListViewAdapter());
 		//Add header.
