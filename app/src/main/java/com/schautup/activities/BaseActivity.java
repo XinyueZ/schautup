6704dcb95a435a92c577e8fcca14a002318cb00a
Xinyue Zhao <czhao@cellular.de>
2014-09-24 20:53:09 +0200

    Added Admob.

--- app/src/main/java/com/schautup/activities/BaseActivity.java
+++ app/src/main/java/com/schautup/activities/BaseActivity.java
@@ -2,7 +2,11 @@ package com.schautup.activities;
 
 import java.lang.reflect.Field;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.TypedArray;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -13,6 +17,8 @@ import android.view.MenuItem;
 import android.view.ViewConfiguration;
 
 import com.chopping.application.BasicPrefs;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.schautup.R;
 import com.schautup.fragments.AboutDialogFragment;
 import com.schautup.utils.Prefs;
@@ -76,9 +82,24 @@ public abstract class BaseActivity extends com.chopping.activities.BaseActivity 
 	@Override
 	public void onResume() {
 		super.onResume();
-		//The "End User License Agreement" must be confirmed before you use this application.
-		if (!Prefs.getInstance(getApplication()).isEULAOnceConfirmed()) {
-			showDialogFragment(AboutDialogFragment.EulaConfirmationDialog.newInstance(this), null);
+		final int isFound = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		if (isFound == ConnectionResult.SUCCESS ||
+				isFound == ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED) {//Ignore update.
+			//The "End User License Agreement" must be confirmed before you use this application.
+			if (!Prefs.getInstance(getApplication()).isEULAOnceConfirmed()) {
+				showDialogFragment(AboutDialogFragment.EulaConfirmationDialog.newInstance(this), null);
+			}
+		}else {
+			new AlertDialog.Builder(this).setTitle(R.string.app_name).setMessage(R.string.lbl_play_service)
+					.setCancelable(false).setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					dialog.dismiss();
+					Intent intent = new Intent(Intent.ACTION_VIEW);
+					intent.setData(Uri.parse(getString(R.string.play_service_url)));
+					startActivity(intent);
+					finish();
+				}
+			}).create().show();
 		}
 	}
 
